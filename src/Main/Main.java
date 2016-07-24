73dd298dc789bf0a279d8c9f015157ef99a87df3
norikoff <yuranorikov@gmail.com>
2016-07-24 14:43:05 +0300

    Second

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+/**
+ * Created by yuran_000 on 24.07.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Worlds,motherfuckers!");
+    }
+}
